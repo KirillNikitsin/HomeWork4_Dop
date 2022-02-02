61a7d7649ef649147cc6c9c25154ddd7dd20b17e
KirillNikitin <kirya19982809@gmail.com>
2022-02-03 02:24:20 +0300

    Initial commit

---
+++ src/main/java/HomeWork4_Dop.java
@@ -0,0 +1,192 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HomeWork4_Dop {
+
+    public static char[][] map;
+    public static final int SIZE = 5;
+    public static final char DOT_EMPTY = '.';
+    public static final char DOT_X = 'X';
+    public static final char DOT_O = 'O';
+    public static Scanner scanner = new Scanner(System.in);
+    public static Random random = new Random();
+
+    public static void initMAP() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+    public static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void humanTurn() {
+        int x;
+        int y;
+        do {
+            System.out.println("Введите координаты X Y");
+            x = scanner.nextInt() - 1;
+            y = scanner.nextInt() - 1;
+        }
+        while (!isCellValid(x, y));
+        map[y][x] = DOT_X;
+    }
+
+    public static void aiTurn() {
+        int x;
+        int y;
+        do {
+            x = random.nextInt(SIZE);
+            y = random.nextInt(SIZE);
+        }
+        while (!isCellValid(x, y));
+        System.out.printf("Компьютер выбрал координаты %s %s\n", x + 1, y + 1);
+        map[y][x] = DOT_O;
+    }
+
+    public static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) {
+            return false;
+        }
+        if (map[y][x] == DOT_EMPTY) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkWin(char symb) {
+        for (int i = 0; i < SIZE; i++) {
+            if (map[i][0] == symb && map[i][1] == symb && map[i][2] == symb && map[i][3] == symb) return true;
+            if (map[i][1] == symb && map[i][2] == symb && map[i][3] == symb && map[i][4] == symb) return true;
+        }
+        for (int j = 0; j < SIZE; j++) {
+            if (map[0][j] == symb && map[1][j] == symb && map[2][j] == symb && map[3][j] == symb) return true;
+            if (map[1][j] == symb && map[2][j] == symb && map[3][j] == symb && map[4][j] == symb) return true;
+        }
+        int colSymbolsL = 0;
+        int colSymbolsR = 0;
+        char symb1 = DOT_X;
+        char symb2 = DOT_O;
+        char symb3 = DOT_EMPTY;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == symb1 && (i == j)) {
+                    colSymbolsL++;
+                } else if (map[i][j] == symb2 && (i == j)) {
+                    colSymbolsL -= 1;
+                } else if (map[i][j] == symb3 && (i == j)) {
+                    if (map[0][0] == symb3) {
+                        colSymbolsL = colSymbolsL + 1 - 1;
+                    } else {
+                        return false;
+                    }
+                }
+                if (colSymbolsL == 4) return true;
+            }
+        }
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == symb2 && (i == j)) {
+                    colSymbolsL++;
+                } else if (map[i][j] == symb1 && (i == j)) {
+                    colSymbolsL -= 1;
+                } else if (map[i][j] == symb3 && (i == j)) {
+                    if (map[0][0] == symb3) {
+                        colSymbolsL = colSymbolsL + 1 - 1;
+                    } else {
+                        return false;
+                    }
+                }
+                if (colSymbolsL == 4) return true;
+            }
+        }
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == symb1 && (SIZE - i - 1 == j)) {
+                    colSymbolsR++;
+                } else if (map[i][j] == symb2 && (SIZE - i - 1 == j)) {
+                    colSymbolsR -= 1;
+                } else if (map[i][j] == symb3 && (SIZE - i - 1 == j)) {
+                    if (map[0][0] == symb3) {
+                        colSymbolsR = colSymbolsL + 1 - 1;
+                    } else {
+                        return false;
+                    }
+                }
+                if (colSymbolsR == 4) return true;
+            }
+        }
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == symb2 && (SIZE - i - 1 == j)) {
+                    colSymbolsR++;
+                } else if (map[i][j] == symb1 && (SIZE - i - 1 == j)) {
+                    colSymbolsR -= 1;
+                } else if (map[i][j] == symb3 && (SIZE - i - 1 == j)) {
+                    if (map[0][0] == symb3) {
+                        colSymbolsR = colSymbolsL + 1 - 1;
+                    } else {
+                        return false;
+                    }
+                }
+                if (colSymbolsR == 4) return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        initMAP();
+        printMap();
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Победил человек");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Победил Искуственный Интеллект");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+        }
+        System.out.println("Игра закончена");
+    }
+}
